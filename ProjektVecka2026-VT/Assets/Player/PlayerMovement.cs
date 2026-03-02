ef9cae5904ae2988df36ca21b6734e279e861c4a
K-M-N <edgar.aberg@gmail.com>
2026-03-02 14:51:09 +0100

    Player på G

---
+++ ProjektVecka2026-VT/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+//Edgar Åberg, 2026-03-02
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerMovement : MonoBehaviour, InputSystem_Actions.IPlayerActions
+{ 
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpHeight;
+    public Vector2 movedirection;
+
+    private Rigidbody rb;
+    private Animator animator;
+    private PlayerInput pInput;
+
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        pInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector3(movedirection.x* moveSpeed*Time.deltaTime, 0, movedirection.y*moveSpeed * Time.deltaTime));
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+        movedirection = context.ReadValue<Vector2>();
+        
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        
+    }
+}
